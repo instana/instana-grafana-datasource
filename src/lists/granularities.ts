053ae5c12ae0fc31b452559054b71f03df650101
Dalje-et <daljeet-sandu@hotmail.de>
2019-07-09 15:19:36 +0200

    move rollup dropdown next to metric dropdown and change labels of granularity and rollups

--- src/lists/granularities.ts
+++ src/lists/granularities.ts
@@ -1,50 +1,50 @@
 export default [
   {
     value: 1, // 1s
-    label: '1s'
+    label: '1s Granularity'
   },
   {
     value: 5, // 5s
-    label: '5s'
+    label: '5s Granularity'
   },
   {
     value: 10, // 10s
-    label: '10s'
+    label: '10s Granularity'
   },
   {
     value: 60, // 1m
-    label: '1min'
+    label: '1min Granularity'
   },
   {
     value: 60 * 5, // 5m
-    label: '5min'
+    label: '5min Granularity'
   },
   {
     value: 60 * 10, // 10m
-    label: '10min'
+    label: '10min Granularity'
   },
   {
     value: 60 * 60, // 1h
-    label: '1h'
+    label: '1h Granularity'
   },
   {
     value: 60 * 60 * 5, // 5h
-    label: '5h'
+    label: '5h Granularity'
   },
   {
     value: 60 * 60 * 10, // 10h
-    label: '10h'
+    label: '10h Granularity'
   },
   {
     value: 60 * 60 * 24, // 1d
-    label: '1d'
+    label: '1d Granularity'
   },
   {
     value: 60 * 60 * 24 * 5, // 5d
-    label: '5d'
+    label: '5d Granularity'
   },
   {
     value: 60 * 60 * 24 * 10, // 10d
-    label: '10d'
+    label: '10d Granularity'
   }
 ];
--- src/lists/rollups.ts
+++ src/lists/rollups.ts
@@ -2,26 +2,26 @@ export default [
   {
     availableFor: 1000 * 60 * 60 * 24, // 1d
     rollup: 1000, // 1s
-    label: '1s'
+    label: '1s Rollup'
   },
   {
     availableFor: 1000 * 60 * 60 * 24, // 1d
     rollup: 1000 * 5, // 5s
-    label: '5s'
+    label: '5s Rollup'
   },
   {
     availableFor: 1000 * 60 * 60 * 24 * 31, // 1 month
     rollup: 1000 * 60, // 1m
-    label: '1min'
+    label: '1min Rollup'
   },
   {
     availableFor: 1000 * 60 * 60 * 24 * 31 * 3, // 3 months
     rollup: 1000 * 60 * 5, // 5m
-    label: '5min'
+    label: '5min Rollup'
   },
   {
     availableFor: Number.MAX_VALUE, // forever
     rollup: 1000 * 60 * 60, // 1h
-    label: '1h'
+    label: '1h Rollup'
   }
 ];
