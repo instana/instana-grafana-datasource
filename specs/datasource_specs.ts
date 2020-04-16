8368891ca30a2657b0c35652c6ed4ffc85074c22
Dalje-et <daljeet-sandu@hotmail.de>
2020-04-16 19:08:04 +0200

    Added tests for SLI information retrieval

--- specs/datasource_specs.ts
+++ specs/datasource_specs.ts
@@ -100,6 +100,144 @@ describe('Given an InstanaDatasource', function() {
     });
   });
 
+  describe('When retrieving SLO information', function() {
+    const definedSli = [
+      {
+        "id": "1",
+        "sloName": "Slo 1",
+        "initialEvaluationTimestamp": 1585046123732,
+        "metricName": "latency",
+        "metricAggregation": "P95",
+        "threshold": 150,
+        "applicationId": "PYmAf835TSKdhR6Nm4gFMQ",
+        "serviceId": null,
+        "endpointId": null,
+        "lastUpdated": 1585066443732
+      },
+      {
+        "id": "2",
+        "sloName": "Slo 2",
+        "initialEvaluationTimestamp": 1585046123732,
+        "metricName": "latency",
+        "metricAggregation": "P95",
+        "threshold": 150,
+        "applicationId": "PYmAf835TSKdhR6Nm4gFMR",
+        "serviceId": null,
+        "endpointId": null,
+        "lastUpdated": 1585066443732
+      },
+      {
+        "id": "3",
+        "sloName": "Slo 3",
+        "initialEvaluationTimestamp": 1585046123732,
+        "metricName": "latency",
+        "metricAggregation": "P95",
+        "threshold": 150,
+        "applicationId": "PYmAf835TSKdhR6Nm4gFMS",
+        "serviceId": null,
+        "endpointId": null,
+        "lastUpdated": 1585066443732
+      } 
+    ];
+
+    const sliReportResponse = {
+      "data": { 
+        "sli":1.0,
+        "slo":0.8,
+        "errorBudgetRemaining":72,
+        "fromTimestamp":1587029766000,
+        "toTimestamp":1587051366000,
+        "violationDistribution": {"0":1,"1":1,"2":1,"3":1,"4":1,"5":1,"6":1,"7":1,"8":1,"9":0,"10":0,"11":0,"12":0,"13":0,"14":0,"15":0,"16":0,"17":0,"18":0,"19":0,"20":0,"21":0,"22":0,"23":0,"24":0,"25":0,"26":0,"27":0,"28":0,"29":0,"30":0,"31":0,"32":1,"33":1,"34":1,"35":1,"36":1,"37":1,"38":1,"39":1,"40":1,"41":1,"42":1,"43":1,"44":1,"45":1,"46":1,"47":1,"48":1,"49":1,"50":1,"51":1,"52":1,"53":1,"54":1,"55":1,"56":1,"57":1,"58":1,"59":1,"60":1,"61":1,"62":1,"63":1,"64":1,"65":1,"66":1,"67":1,"68":1,"69":1,"70":1,"71":1,"72":1,"73":1,"74":1,"75":1,"76":1,"77":0,"78":0,"79":-1,"80":-1,"81":-1,"82":-1,"83":-1,"84":-1,"85":-1,"86":-1,"87":-1,"88":-1,"89":-1,"90":-1,"91":-1,"92":-1,"93":-1,"94":0,"95":0,"96":0,"97":0,"98":0,"99":0,"100":0}
+      }
+    };
+
+    beforeEach(function() {
+      ctx.backendSrv.datasourceRequest = function(options) {
+        switch (options.url) {
+          case "/api/datasources/proxy/1/instana/api/settings/sli":
+            return ctx.$q.resolve(definedSli);
+          case "/api/datasources/proxy/1/instana/api/sli/report/2?from=1516451043603&to=1516472658604&slo=0.8":
+            return ctx.$q.resolve(sliReportResponse);
+          default:
+            throw new Error('Unexpected call URL: ' + options.url);
+        }
+      };
+    });
+
+    it('should return only one value for sli', function() {
+      return ctx.ds.slo.fetchSLOReport({
+        "sloReport": {
+          "key": "2",
+          "label": "Some Random SloReport"
+        },
+        "sloSpecific": {
+          "key": "SLI",
+          "label": "SLI"
+        },
+        "sloValue": "0.8"
+      }, timeFilter)
+      .then(function(results) {
+        expect(results.length).to.equal(1);
+        expect(results[0].target).to.equal("SLI");
+        expect(results[0].datapoints.length).to.equal(1);
+        expect(results[0].datapoints[0][0]).to.equal(1.0);
+      });
+    });
+
+    it('should return only one value for remaining error budget', function() {
+      return ctx.ds.slo.fetchSLOReport({
+        "sloReport": {
+          "key": "2",
+          "label": "Some Random SloReport"
+        },
+        "sloSpecific": {
+          "key": "Remaining Error Budget",
+          "label": "Remaining Error Budget"
+        },
+        "sloValue": "0.8"
+      }, timeFilter)
+      .then(function(results) {
+        expect(results.length).to.equal(1);
+        expect(results[0].target).to.equal("Remaining Error Budget");
+        expect(results[0].datapoints.length).to.equal(1);
+        expect(results[0].datapoints[0][0]).to.equal(72);
+      });
+    });
+
+    it('should return three result targets with only datapoint values of 1', function() {
+      return ctx.ds.slo.fetchSLOReport({
+        "sloReport": {
+          "key": "2",
+          "label": "Some Random SloReport"
+        },
+        "sloSpecific": {
+          "key": "Timeseries",
+          "label": "Timeseries"
+        },
+        "sloValue": "0.8"
+      }, timeFilter)
+      .then(function(results) {
+        expect(results[0].length).to.equal(3);
+        expect(results[0][0].target).to.equal("No violation");
+        expect(results[0][1].target).to.equal("Violation");
+        expect(results[0][2].target).to.equal("No data");
+
+        for (let j = 0; j < results[0][0].datapoints.length; j++) {
+          expect(results[0][0].datapoints[j][0]).to.equal(1);
+        }
+
+        for (let j = 0; j < results[0][1].datapoints.length; j++) {
+          expect(results[0][1].datapoints[j][0]).to.equal(1);
+        }
+
+        for (let j = 0; j < results[0][2].datapoints.length; j++) {
+          expect(results[0][2].datapoints[j][0]).to.equal(1);
+        }
+      });
+    });
+
+  });
+
   describe('When retrieving snapshots for a target', function() {
 
     const contexts = {
@@ -195,7 +333,7 @@ describe('Given an InstanaDatasource', function() {
       });
     });
 
-    xit('should cache snapshot data for a minute', function() {
+    it('should cache snapshot data for a minute', function() {
       ctx.ds.infrastructure.currentTime = () => { return 1516472658604; };
 
       return ctx.ds.infrastructure.fetchSnapshotsForTarget({
