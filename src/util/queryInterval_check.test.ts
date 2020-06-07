d771dc39b272c764cbac5b3a71d2c3ec013c1ed7
Daljeet Sandu <daljeet-sandu@hotmail.de>
2020-06-07 13:27:08 +0200

    implemented test for queryInterval check

---
+++ src/util/queryInterval_check.test.ts
@@ -0,0 +1,40 @@
+import { isInvalidQueryInterval } from './queryInterval_check';
+
+describe('Given no queryInterval limit', function() {
+  it('should mark the queryInterval as invalid (return false)', function() {
+    let windowSize: number = 16091997;
+    expect(isInvalidQueryInterval(windowSize, undefined)).toEqual(false);
+  });
+});
+
+describe('Given a query interval limit that equals 0', function() {
+  it('should mark the queryInterval as invalid (return false)', function() {
+    let windowSize: number = 16091997;
+    expect(isInvalidQueryInterval(windowSize, 0)).toEqual(false);
+  });
+});
+
+describe('Given a query interval limit that is 1 ms smaller than windowSize', function() {
+  it('should mark the queryInterval as valid (return true)', function() {
+    let windowSize: number = 10000;
+    let queryIntervalLimit: number = 9999;
+    expect(isInvalidQueryInterval(windowSize, queryIntervalLimit)).toEqual(true);
+  });
+});
+
+describe('Given a query interval limit that is equals windowSize', function() {
+  it('should mark the queryInterval as invalid (return false)', function() {
+    let windowSize: number = 10000;
+    let queryIntervalLimit: number = 10000;
+    expect(isInvalidQueryInterval(windowSize, queryIntervalLimit)).toEqual(false);
+  });
+});
+
+describe('Given a query interval limit that is larger than windowSize', function() {
+  it('should mark the queryInterval as invalid (return false)', function() {
+    let windowSize: number = 10000;
+    let queryIntervalLimit: number = 10001;
+    expect(isInvalidQueryInterval(windowSize, queryIntervalLimit)).toEqual(false);
+  });
+});
+
