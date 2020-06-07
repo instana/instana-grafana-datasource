750660dbc1526d52889bfb0999ce954b9fdd67ab
Daljeet Sandu <daljeet-sandu@hotmail.de>
2020-06-07 13:29:14 +0200

    added cache test

---
+++ src/cache.test.ts
@@ -0,0 +1,27 @@
+import Cache from '../src/cache';
+
+describe('Given a cache', function() {
+  let cache: Cache<any>;
+  beforeEach(() => {
+    cache = new Cache();
+  });
+
+  describe('with ttl', function() {
+    beforeEach(() => {
+      cache.put('key', 'value', 100);
+    });
+
+    it('should contain the value after it was added', function() {
+      const value = cache.get('key');
+      expect(value).toEqual('value');
+    });
+
+    it('should not contain the value after ttl has passed', done => {
+      setTimeout(() => {
+        const value = cache.get('key');
+        expect(value).toEqual(undefined);
+        done();
+      }, 150);
+    });
+  });
+});
