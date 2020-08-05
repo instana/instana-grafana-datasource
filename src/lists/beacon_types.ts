500a1558979d191fbf1f56228fb644b416cf8a62
Daljeet Sandu <daljeet-sandu@hotmail.de>
2020-08-05 10:22:10 +0200

    add new beacon types

--- src/lists/beacon_types.ts
+++ src/lists/beacon_types.ts
@@ -1,6 +1,8 @@
 export default [
   { key: 'pageLoad', label: 'Page Loads' },
+  { key: 'pageChange', label: 'Page Transitions' },
   { key: 'resourceLoad', label: 'Resources' },
   { key: 'httpRequest', label: 'HTTP Requests' },
   { key: 'error', label: 'Errors' },
+  { key: 'custom', label: 'Custom Events' },
 ];
