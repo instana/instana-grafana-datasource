1b8087efc16bf214771c4e5bfbf30ec5706463e9
Daljeet Sandu <daljeet-sandu@hotmail.de>
2020-09-07 17:31:11 +0200

    fix wrong beacon type key

--- src/lists/beacon_types.ts
+++ src/lists/beacon_types.ts
@@ -1,6 +1,6 @@
 export default [
   { key: 'pageLoad', label: 'Page Loads' },
-  { key: 'pageChange', label: 'Page Transitions' },
+  { key: 'page_change', label: 'Page Transitions' },
   { key: 'resourceLoad', label: 'Resources' },
   { key: 'httpRequest', label: 'HTTP Requests' },
   { key: 'error', label: 'Errors' },
