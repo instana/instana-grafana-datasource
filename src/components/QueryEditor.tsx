f927566835a088d1f00d09749f9e1d7abfb3250b
Daljeet Sandu <daljeet-sandu@hotmail.de>
2021-01-11 11:37:19 +0100

    Bumped version to 3.1.3

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -483,7 +483,7 @@ export class QueryEditor extends PureComponent<Props, QueryState> {
           loadEntityTypes={this.loadEntityTypes}
         />
 
-        <Badge text={'3.1.2'} color={'blue'} />
+        <Badge text={'3.1.3'} color={'blue'} />
       </div>
     );
   }
