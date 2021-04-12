a3c1f14ed01a11fce7bd1acbf747254e21a8c2df
Daljeet Sandu <daljeet-sandu@hotmail.de>
2021-04-12 14:53:09 +0200

    forward error properly from infra explore call

--- src/datasources/Datasource_Infrastructure.ts
+++ src/datasources/Datasource_Infrastructure.ts
@@ -211,6 +211,11 @@ export class DataSourceInfrastructure {
     return postRequest(this.instanaOptions, '/api/infrastructure-monitoring/explore/groups', payload).then(
       (res: any) => {
         let result: any = [];
+
+        if (!res.data.data && res.data.errors.length >= 1) {
+          throw new Error(res.data.errors[0].message || res.data.errors[0]);
+        }
+
         res.data.data.items.forEach((entity: any) => {
           for (var metric in entity.metrics) {
             result.push({
