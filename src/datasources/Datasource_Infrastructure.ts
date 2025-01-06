0a10b4235ae07823f6356ac16e5d0ae18b199e31
Sarath S R <140109309+Sarathsgck12@users.noreply.github.com>
2025-01-06 18:39:24 +0530

    Fixed the API response limit issue in both Built-in and Custom metric category and Legend format issue (#148)
    * Fixed the API response limit issue in both Built-in and Custom metric category
    
    * fix legend format issue

--- src/datasources/Datasource_Infrastructure.ts
+++ src/datasources/Datasource_Infrastructure.ts
@@ -97,35 +97,45 @@ export class DataSourceInfrastructure {
 
   fetchMetricsForSnapshots(target: InstanaQuery, snapshots: any, timeFilter: TimeFilter, metric: any) {
     let maxValues: any = [];
-    let snapshotIds: string[] = [];
-    _.map(snapshots, (snapshot) => snapshotIds.push(snapshot.snapshotId));
-    // ...fetch the metric data for every snapshot in the results.
-    return this.fetchMetricsForSnapshot(target, snapshotIds.slice(0, 30), timeFilter, metric).then((response: any) => {
-      if (!response.data) {
-        return response;
-      }
-      let timeseries = this.readTimeSeries(response.data.items, target.aggregation, timeFilter);
-      // as we map two times we need to flatten the result
-      let results = _.flatten(
-        response.data.items.map((item: any, index: number) => {
-          return _.map(item.metrics, (value, key) => {
-            let result = {
-              target: item.label,
-              datapoints: _.map(value, (metric) => [metric[1], metric[0]]),
-              refId: target.refId,
-              key: target.stableHash,
-            };
-            if (target.displayMaxMetricValue) {
-              const maxValue = this.getMaxMetricValue(target.metric, snapshots);
-              maxValues.push(this.buildMaxMetricTarget(target, timeseries, maxValue, result.target));
-              result.datapoints = this.convertRelativeToAbsolute(result.datapoints, maxValue);
-            }
-            return result;
-          });
-        })
-      );
-      return results;
+    let snapshotPromises = snapshots.map((snapshot: { snapshotId: any; response: any; host: any }, index: number) => {
+      const snapshotId = snapshot.snapshotId;
+      const snapshotResponse = snapshot.response;
+      const host = snapshot.host;
+
+      // Call fetchMetricsForSnapshot for each snapshot
+      return this.fetchMetricsForSnapshot(target, [snapshotId], timeFilter, metric).then((response: any) => {
+        if (!response.data) {
+          return [];
+        }
+
+        let timeseries = this.readTimeSeries(response.data.items, target.aggregation, timeFilter);
+        return _.flatten(
+          response.data.items.map((item: any) => {
+            return _.map(item.metrics, (value, key) => {
+              let label = this.buildLabel(snapshotResponse, host, target, index, metric);
+
+              let result = {
+                target: label,
+                datapoints: _.map(value, (metric) => [metric[1], metric[0]]),
+                refId: target.refId,
+                key: target.stableHash,
+              };
+
+              if (target.displayMaxMetricValue) {
+                const maxValue = this.getMaxMetricValue(target.metric, snapshots);
+                maxValues.push(this.buildMaxMetricTarget(target, timeseries, maxValue, label));
+                result.datapoints = this.convertRelativeToAbsolute(result.datapoints, maxValue);
+              }
+
+              return result;
+            });
+          })
+        );
+      });
     });
+
+    // Wait for all promises to complete and flatten the results
+    return Promise.all(snapshotPromises).then((allResults) => _.flatten(allResults));
   }
 
   getMaxMetricValue(metric: any, snapshot: any): number {
