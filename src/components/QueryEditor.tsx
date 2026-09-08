b350ef933a36b8feb3ab8ab96d5de0d3c918ba57
Sarath-S-R <sarath.s.r@ibm.com>
2026-09-08 16:05:40 +0530

    Bumped version to 5.2.0

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -741,7 +741,7 @@ export class QueryEditor extends PureComponent<Props, QueryState> {
           <AdvancedSettings query={query} onRunQuery={this.props.onRunQuery} onChange={this.props.onChange} />
         )}
 
-        <Badge text={'5.1.0'} color={'blue'} />
+        <Badge text={'5.2.0'} color={'blue'} />
       </div>
     );
   }
